8ce2396de174306d396d0d2b0c1c9e08cf2c77fa
Samuel Farto Vaamonde <sfartovaamonde@danielcastelao.org>
2021-12-14 10:38:48 +0100

    Programación

---
+++ src/miprimerproyecto/Boletin2_1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimerproyecto;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin2_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        float base=4.0f,altura=3f;
+  
+        float area=(base*altura)/2;
+    
+        System.out.println("o area do triangulo é="+area);
+        
+        
+
+
+
+
+// TODO code application logic here
+    }
+    
+}
